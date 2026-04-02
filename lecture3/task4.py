dd41cdbdaf14445ccb5bb5ffc1d841272cb033d7
musfraijaz <ah5981035@yahoo.com>
2026-04-02 16:50:05 +0500

    task done

---
+++ lecture3/task4.py
@@ -0,0 +1,16 @@
+import random
+
+number = random.randint(1, 10)
+
+guess = None
+
+
+while guess != number:
+    guess = int(input("Guess a number between 1 and 10: "))
+
+    if guess > number:
+        print("Too high")
+    elif guess < number:
+        print("Too low")
+    else:
+        print("Correct! You guessed the number.")
